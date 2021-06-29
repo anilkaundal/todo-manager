1b5fbd604ee56d43f3efb82130fef94f495427ad
Anil Kaundal <anilkaundal1999@gmail.com>
2021-06-29 16:57:31 +0530

    Create a new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,13 @@ class UsersController < ApplicationController
   def index
     render plain: User.all.map { |todo| todo.to_pleasant_string }.join("\n")
   end
+
+  def create
+    name = params[:name]
+    email = params[:email]
+    password = params[:password]
+    input = (name == nil || email == nil || password == nil) ? nil : User.create!(name: name, email: email, password: password)
+    response_text = input == nil ? "Input can't be empty!! " : "Hey, your account is created with id #{input.id}"
+    render plain: response_text
+  end
 end
