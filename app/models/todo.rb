c0b1a373b5f5227a7f954552ff4f1ad4a27afe30
Anil Kaundal <anilkaundal1999@gmail.com>
2021-07-04 19:30:43 +0530

    Combine the design-based HTML & CSS with ERB

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -3,4 +3,31 @@ class Todo < ActiveRecord::Base
     is_completed = completed ? "[x]" : "[ ]"
     "[#{id}]. #{due_date.to_s(:short)} #{todo_text}  #{is_completed}"
   end
+
+  def due_today?
+    due_date == Date.today
+  end
+
+  def self.overdue
+    where("due_date < ? and (not completed)", Date.today).order(:due_date)
+  end
+
+  def self.due_today
+    all.where("due_date = ?", Date.today)
+  end
+
+  def self.due_later
+    all.where("due_date > ?", Date.today)
+  end
+
+  def self.completed
+    all.where(completed: true)
+  end
+
+  def self.mark_as_complete(id)
+    to_mark = Todo.find(id)
+    to_mark.completed = true
+    to_mark.save
+    to_mark
+  end
 end
