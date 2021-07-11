14bc1ef5079ab29276101e050f6f097bd7861540
Anil Kaundal <anilkaundal1999@gmail.com>
2021-07-12 01:21:21 +0530

    Ensure no blank todos are ever created, with ActiveRecord validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  validates :first_name, :email, :password, presence: true
+  validates :password, length: { minimum: 8 }
+
   has_secure_password
   has_many :todos
 
